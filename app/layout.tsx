68c8dd3dab906b89cb85a76c645836fede1ab2cb
Matthew J. Whitney <matt@bedda.tech>
2025-11-09 00:51:23 +0100

    fix: set HTML lang attribute to 'it' for Chrome translation detection

--- app/layout.tsx
+++ app/layout.tsx
@@ -68,7 +68,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="it" suppressHydrationWarning>
       <head>
         <meta name="mobile-web-app-capable" content="yes" />
         <script
